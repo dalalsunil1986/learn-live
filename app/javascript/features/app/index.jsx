29be7a552b14338780edf0d2fe383e879adf9b7a
Mark Tellez <mark.tellez@icloud.com>
2019-02-02 00:47:31 -0600

    basic actioncable wiring

--- app/javascript/features/app/index.jsx
+++ app/javascript/features/app/index.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import LandingPage from '../landing-page'
+import SignUp from '../sign-up'
 
 export default function App({}) {
-  return <div>Hello from React</div>
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/sign-up" component={SignUp} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
--- app/javascript/packs/application.jsx
+++ app/javascript/packs/application.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ActionCable from 'actioncable-modules'
 
 import App from '../features/app'
 
+const meta = Array.from(document.getElementsByTagName('meta')).find((el) => {
+  return el.getAttribute('name') === 'action-cable-url'
+})
+let cable = ActionCable.createConsumer(meta.getAttribute('content'))
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <App cable={cable} />,
     document.body.appendChild(document.createElement('div')),
   )
 })
